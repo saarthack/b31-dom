e2417ad8d9a1c5e5a0e5d9b27d79d36a109c442f
saarthack <ssarthak2003@outlook.com>
2025-04-08 18:28:31 +0530

    kewda thakur

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-var btn = document.querySelector('button')
-var body = document.body
-btn.addEventListener('click',function(){
-    var img = document.createElement('img')
+var arr = [
+    {
+        username:'Heera',
+        buses:50,
+        likesKheer:false,
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUlRH8Ce0wwoOKFo2594oJzLsXy34HAQV0wQ&s'
+    },
+    {
+        username:'Bhanu Pratap',
+        buses:0 ,
+        likesKheer:true,
+        image:'https://im.indiatimes.in/content/2020/May/kheer_5ec3a4e63976a.png?w=375&h=210&cc=1&webp=1&q=75'
+    },
+    {
+        username:'Kewda Thakur',
+        buses:5,
+        likesKheer:true,
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfJVfTiNvpDBSvOga254qEZ_Upw0b2T9glQw&s'
+    },
+]
 
-    var x = Math.random()*100
-    var y = Math.random()*100
-    var rot = Math.random()*360
-    img.style.left = x+'%'
-    img.style.top = y+'%'
-    img.style.rotate = rot+'deg'
+var sum = ''
 
-    img.setAttribute('src','https://www.freeiconspng.com/uploads/spongebob-cartoon-png-32.png')
-    body.appendChild(img)
+arr.forEach(function(elem){
+
+    sum += `<div class="card">
+    <img src="${elem.image}" alt="">
+    <h1>${elem.username}</h1>
+    <h5>Buses : ${elem.buses}</h5>
+    <h4>Likes Kheer : ${elem.likesKheer}</h4>
+</div>`
 })
+
+var body = document.querySelector('body')
+
+body.innerHTML = sum
