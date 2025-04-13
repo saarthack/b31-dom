8076930976e06f06bdd40b9a971e06eefe5aee3b
saarthack <ssarthak2003@outlook.com>
2025-04-13 12:57:19 +0530

    final project

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const users = [
+const arr = [
     {
         fullName: "Tony Stark",
         available: true,
@@ -27,7 +27,7 @@ const users = [
         company: "Wakanda Design Group",
         skills: ["Vibranium Tech", "UI/UX", "Python", "Embedded Systems", "AR/VR", "Holography"],
         description: "Wakanda's tech prodigy and innovation powerhouse.",
-        profilePhoto: "https://example.com/profiles/shuri.jpg"
+        profilePhoto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkI13JiXtxseX3VhxUK90FmgI6CoIcovAisg&s"
     },
     {
         fullName: "Peter Parker",
@@ -47,7 +47,7 @@ const users = [
         company: "Fantastic Labs",
         skills: ["Rocket Propulsion", "Multiverse Theory", "Programming", "Math Modeling", "Scientific Research"],
         description: "Stretching boundaries of science and space.",
-        profilePhoto: "https://example.com/profiles/reed.jpg"
+        profilePhoto: "https://res.cloudinary.com/jerrick/image/upload/v1738723692/67a2d16cfe82da001d539795.jpg"
     },
     {
         fullName: "Barbara Gordon",
@@ -57,7 +57,7 @@ const users = [
         company: "Gotham Tech",
         skills: ["Cybersecurity", "Hacking", "Python", "Network Defense", "Forensics", "Databases"],
         description: "Oracle of the digital world, crime-fighting coder.",
-        profilePhoto: "https://example.com/profiles/barbara.jpg"
+        profilePhoto: "https://static.dc.com/dc/files/default_images/BabsCasting_60073b823ebff3.73076079.jpg"
     },
     {
         fullName: "Victor Stone",
@@ -103,10 +103,10 @@ const users = [
 
 var sum = ''
 
-users.forEach(function(elem,idx){
+arr.forEach(function (elem, idx) {
     sum = sum + `<div class="card">
     <div class="top">
-        ${elem.available?'<button>available</button>':'<h1></h1>'}
+        ${elem.available ? '<button>available</button>' : '<h1></h1>'}
         <h3>$${elem.fees}/hr</h3>
     </div>
     <img src="${elem.profilePhoto}" alt="">
@@ -123,7 +123,7 @@ users.forEach(function(elem,idx){
         <h4>${elem.skills[0]}</h4>
         <h4>${elem.skills[1]}</h4>
         <h4>${elem.skills[2]}</h4>
-        <h5>+${elem.skills.length-3}</h5>
+        <h5>+${elem.skills.length - 3}</h5>
     </div>
 
     <p>${elem.description}</p>
@@ -137,18 +137,76 @@ var main = document.querySelector('#main')
 main.innerHTML = sum
 
 var full = document.querySelector('#full')
-var fullImg = document.querySelector('#full img')
-var fullname = document.querySelector('#full h1')
+var gold = 0
 
-main.addEventListener('click',function(dets){
-    var gold = users[dets.target.id]
+var fullImageIndex = 0
 
-    full.style.display = 'flex'
-    fullImg.src = gold.profilePhoto
-    fullname.innerHTML = gold.fullName
+function showFullScreen() {
+    gold = arr[fullImageIndex]
+
+
+    full.style.display = 'block'
+
+    var left = full.childNodes[3].childNodes[1]
+    var right = full.childNodes[3].childNodes[3]
+
+    right.childNodes[1].src = gold.profilePhoto
+    left.childNodes[1].childNodes[1].innerHTML = gold.fullName
+    left.childNodes[1].childNodes[3].innerHTML = `$${gold.fees}/hr`
+    left.childNodes[3].innerHTML = gold.profession
+    left.childNodes[5].innerHTML = gold.company
+    left.childNodes[9].innerHTML = gold.description
+    left.childNodes[11].innerHTML = gold.available ? 'HIRE' : 'Not Available'
+    if (gold.available) {
+        left.childNodes[11].style.opacity = 1
+    } else {
+        left.childNodes[11].style.opacity = 0.3
+    }
+
+    var clutter = ''
+    gold.skills.forEach(function(elem){
+        clutter = clutter + `<h4>${elem}</h4>`
+    })
+    left.childNodes[7].innerHTML = clutter
+}
+
+main.addEventListener('click', function (dets) {
+    fullImageIndex = Number(dets.target.id)
+
+    showFullScreen()
+
 
 })
 
-document.querySelector('#close').addEventListener('click',function(){
+
+
+
+var cross = document.querySelector('#cross')
+cross.addEventListener('click', function () {
     full.style.display = 'none'
+})
+
+var next = document.querySelector('#next')
+var prev = document.querySelector('#prev')
+
+
+next.addEventListener('click', function () {
+
+    if (fullImageIndex < arr.length) {
+        fullImageIndex++
+        showFullScreen()
+    }
+})
+
+prev.addEventListener('click', function () {
+
+    if (fullImageIndex > 0) {
+        prev.style.opacity = 1
+
+        fullImageIndex--
+        showFullScreen()
+
+    } else {
+        prev.style.opacity = 0.4
+    }
 })
